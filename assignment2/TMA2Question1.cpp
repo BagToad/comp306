45cb57ca5ff65e422db668f5f88c2376d1f86b33
UtilizedNoodle <kware@protonmail.com>
2021-09-30 10:29:29 -0600

    added assignment 2

---
+++ assignment2/TMA2Question1.cpp
@@ -0,0 +1,59 @@
+/* 
+ Title: TMA2Question1.cpp
+ Description:
+    Open a text file and count whitespace separated words.
+ Date: August 13, 2021
+ Author: Kynan Ware
+ Student ID: 3431123 
+ Compile: g++ TMA2Question1.cpp -o TMA2Question1.exe
+ Execution: ./TMA2Question1.exe
+ 
+ Classes: none
+
+ Variables:
+
+*/
+
+/*
+ TEST PLAN
+ 
+ All test plans, unless noted, require an input file. 
+
+
+ Normal case 1:
+ 
+
+ Normal case 2:
+   
+
+ Normal case 3:
+  
+
+ Bad Data case 1:
+ 
+
+ Bad Data case 2 (illegal characters in filename input)
+
+
+ Bad Data case 3 (empty file)
+  
+ 
+ Bad Data case 4 (file with newlines and special characters)
+   
+
+ 
+ Discussion:
+ 	
+
+    The program returns an int as per the C++ standard.
+
+    All testing performed in a Linux environment with g++ version 7.5.0.
+*/
+
+using namespace std;
+
+int main(void) {
+	
+    return 0;
+}
+
