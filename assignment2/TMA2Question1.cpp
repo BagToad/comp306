a9019d8b78b86da1f082b22db78f9e7c5f802b0e
kynan <kware@protonmail.com>
2021-09-30 13:08:12 -0600

    p

--- assignment2/TMA2Question1.cpp
+++ assignment2/TMA2Question1.cpp
@@ -50,10 +50,132 @@
     All testing performed in a Linux environment with g++ version 7.5.0.
 */
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+
 using namespace std;
 
+class DVD {
+	bool available;
+	string last_rent_date;
+	int year;
+	string title;
+
+public:
+	DVD() {};
+
+	DVD(string t, int y) {
+		available = true;
+		last_rent_date = "none";
+		year = y;
+		title = t;
+	};
+
+	/*
+		Get the DVD availability.
+		
+		Returns:
+		bool - false if DVD unavailable, true if DVD available.
+
+		Arguments:
+		none.
+	*/
+	bool get_available() {
+		return available;
+	};
+
+	/*
+		Set the DVD availability. Effectively checkout or return DVDs to the store.
+		
+		Returns:
+		void
+
+		Arguments:
+		a - bool - true if DVD is returned, false if DVD is being checked out. 
+	*/
+	void set_available(bool a) {
+		if (! a) {
+			time_t now = time(0);
+			last_rent_date = ctime(&now);
+		}
+		available = a;
+	};
+
+	/*
+		Get the last time the DVD was rented out.
+
+		Returns:
+		string - A string with the last time the DVD was checked out. 
+	*/
+	string get_last_rent_date() {
+		return last_rent_date;
+	}
+
+	/*
+		Display the DVD's properties to the terminal.
+
+		Returns:
+		void.
+	*/
+	void print() {
+		cout << "=== Printing DVD information ===" << endl;
+		cout << "title: " << title << endl;
+		cout << "year: " << year << endl;
+		cout << "available: ";
+		if (available) {
+			cout << "Yes";
+		} else {
+			cout << "No";
+		}
+		cout << endl;
+		cout << "last rent date: " << last_rent_date << endl;
+	}
+
+};
+
+
+
 int main(void) {
-	
+	ifstream in;
+    string filename;
+
+	//Get the filename to open from the user.
+	cout << "Filename to open: ";
+	cin >> filename;
+	in.open(filename);
+
+    //Check if input file exists.
+    if (! in) {
+    	cout << "File does not exist!" << endl;
+    	return 1;
+    }
+
+
+	DVD inventory[10];
+	int count = 0;
+	for (int i = 0; i < 9; i++) {
+		string title;
+		int year;
+		cout << "LOADING DVD!" << endl;
+
+
+		if (in.peek() == EOF) {break;}
+		if (! getline(in, title)) {break;};
+		if (in.peek() == EOF) {break;};
+		in >> year;
+		if (in.peek() == EOF) {break;};
+		in.ignore();
+		count++;
+
+		inventory[i] = DVD(title, year);
+	}
+
+	for (int i = 0; i < count; i++) {
+		inventory[i].print();
+	}
+
     return 0;
 }
 
