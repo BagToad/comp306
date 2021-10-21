504df1606a19d077aa7ae0e5dc67046dff1a760a
kynan <kware@protonmail.com>
2021-10-21 15:20:17 -0600

    p

--- assignment3/TMA3Question2.cpp
+++ assignment3/TMA3Question2.cpp
@@ -1,20 +1,22 @@
 /* 
  Title: TMA3Question2.cpp
  Description:
-    Track DVD information. Import DVD data from a text file.
- Date: October 4, 2021
+    Build two arrays of 10,000 doubles, multiply them, and print the result.
+    Also prints the program duration.
+ Date: October 21, 2021
  Author: Kynan Ware
  Student ID: 3431123 
  Compile: g++ TMA3Question2.cpp -o TMA3Question2.exe
  Execution: ./TMA3Question2.exe
  
- Classes:
-
  Variables:
- in - fstream - a stream to read the text file. 
+ 	double - first_doubles[num_doubles] - The first array of doubles.
+	double - second_doubles[num_doubles] - The first array of doubles.
 
  Constants:
-
+	 int - num_doubles - The number of doubles in each double array.
+	 high_resolution_clock::time_point - start_time - The time the program started in milliseconds.
+	 high_resolution_clock::time_point - stop_time - The time the program completed multiplication in milliseconds.
 
 */
 
@@ -50,17 +52,14 @@
     All testing performed in a Linux environment with g++ version 7.5.0.
 */
 
-#include <iostream>
-#include <chrono>
+#include <iostream> //iostream provides io.
+#include <chrono> //chrono provides accurate time tracking.
 
 using namespace std;
 using namespace std::chrono;
 
-inline int output (double first_double, double second_double) __attribute__((always_inline));
-
-inline int output(double first_double, double second_double) {
-	// int out = first_double * second_double;
-	// cout << first_double << " * " << second_double << " = " << out << endl;
+//Multiply two doubles then return the result.
+int calc (double first_double, double second_double) {
 	return first_double * second_double;
 }
 
@@ -71,17 +70,17 @@ int main(void) {
 	double first_doubles[num_doubles];
 	double second_doubles[num_doubles];
 
+	//Build the double arrays.
 	for (int i = 0; i <= num_doubles; i++) {
 		first_doubles[i] = i + 100;
 		second_doubles[i] = 10100 - i;
 	}
-	int out;
-	// for (int i = 0; i <= 100; i++){
-		for (int i = 0; i <= 10000; i++) {
-			out = output(first_doubles[i], second_doubles[i]);
-			cout << "output: " << out << endl;
-		}
-	// }
+
+	//Execute function to sum and print result. 
+	for (int i = 0; i <= num_doubles; i++) {
+		cout << first_doubles[i] << " * " << second_doubles[i] << " = " << calc(first_doubles[i], second_doubles[i]) << endl;
+	}
+
 	//Adapted from http://www.cplusplus.com/reference/chrono/high_resolution_clock/now/
 	const high_resolution_clock::time_point stop_time = high_resolution_clock::now();
 	duration<double> time_span = duration_cast<duration<double>>(stop_time - start_time);
@@ -89,4 +88,3 @@ int main(void) {
 	cout << "\n DURATION: "	<< time_span.count() << endl;
 	return 0;
 }
-
