71bc09de2f85f4c6e957a2d17c6cd971a1c71a5c
Noodle <47394200+UtilizedNoodle@users.noreply.github.com>
2021-10-26 16:23:27 -0600

    Update TMA3Question3.cpp

--- assignment3/TMA3Question3.cpp
+++ assignment3/TMA3Question3.cpp
@@ -74,61 +74,73 @@ public:
 		value = x;
 	}
 
+	//Print value to ostream.
 	void print(ostream& os) {
-		os << "value: " << value << endl;
+		os << value;
 	}
 
+	//Binary addition operator.
 	const DoStuff operator+(const DoStuff& x) const {
 		return DoStuff(value + x.value);
 	}
 
+	//Binary subtraction operator.
 	const DoStuff operator-(const DoStuff& x) const {
 		return DoStuff(value - x.value);
 	}
 
+	//Prefix increment operator. 
 	const DoStuff operator++(int) {
 		DoStuff beforeValue(value);
 		value++;
 		return beforeValue;
 	}
 
+	//Postfix increment operator.
 	const DoStuff operator++() {
 		value++;
 		return *this;
 	}
 
+	//Prefix decrement operator. 
 	const DoStuff operator--(int) {
 		DoStuff beforeValue(value);
 		value--;
 		return beforeValue;
 	}
 
+	//Postfix decrement operator. 
 	const DoStuff operator--() {
 		value--;
 		return *this;
 	}
 
+	//ostream output operator.
 	friend ostream& operator<<(ostream& os, const DoStuff& s) {
 		return os << to_string(s.value);
 	}
 };
 
 int main(void) {
+
+	//Create test instances. 
 	DoStuff a1(5);
 	DoStuff b1(10);
 	DoStuff c1(15);
 
+	//Test + and - operators. 
 	cout << "=== Testing overloaded + and - operators ===" << endl;
 	cout << "Values: a = 5, b = 10, c = 15" << endl;
 	cout << "\nExpression: a + b - c + a" << endl;
 	cout << "Result: " << (a1 + b1 - c1 + a1) << endl;
 
+	//Create test instances. 
 	DoStuff a2(5);
 	DoStuff b2(5);
 	DoStuff c2(5);
 	DoStuff d2(5);
 
-	//Test prefix and postfix operators. 
+	//Test prefix/postfix ++ and -- operators. 
 	cout << "\n=== Testing postfix/prefix overloaded ++ and -- operators ===" << endl;
 	cout << "Values: a = 5, b = 5, c = 5, d = 5" << endl;
 	cout << "\nExpression: a++" << endl;
@@ -140,10 +152,11 @@ int main(void) {
 	cout << "\nExpression: --d" << endl;
 	cout << "Result: " << --d2 << endl;
 
-
+	//Create test instances.
 	DoStuff a3(5);
 	DoStuff b3(15);
 
+	//Test << operator. 
 	cout << "\n===Testing overloaded << operator ===" << endl;
 	cout << "Values: a = 5" << endl;
 	cout << "\nExpression: cout \"a = \" << a << \", b = \" << b << endl;" << endl;
