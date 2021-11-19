54442cdf3ec032a53484b4abb164954f0d3fdf93
UtilizedNoodle <kware@protonmail.com>
2021-11-19 09:20:48 -0700

    add assignment 4

---
+++ assignment4/TMA4Question3.cpp
@@ -0,0 +1,39 @@
+/* 
+ Title: TMA4Question3.cpp
+ Description:
+  
+ Date: Nov 18, 2021
+ Author: Kynan Ware
+ Student ID: 3431123
+ Compile: g++ TMA4Question3.cpp -o TMA4Question3.exe
+ Execution: ./TMA4Question3.exe
+ 
+ Variables:
+
+
+ Constants:
+
+
+*/
+
+/*
+ TEST PLAN
+ 
+ Discussion:
+	The program loads two double arrays with 10,000 values. The values 
+	are multiplied by eachother, then printed. The execution time of 
+	the program is printed, then the program exits.
+	
+   The program returns an int as per the C++ standard.
+
+   All testing performed in a Linux environment with g++ version 7.5.0.
+*/
+
+#include <iostream> //iostream provides io.
+
+using namespace std;
+
+int main(void) {
+	
+	return 0;
+}
