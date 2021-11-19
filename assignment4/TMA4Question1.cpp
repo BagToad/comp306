3f55c1d1e72be27630e6d36f38890cb45751fe73
kynan <kware@protonmail.com>
2021-11-19 10:17:17 -0700

    p

--- assignment4/TMA4Question1.cpp
+++ assignment4/TMA4Question1.cpp
@@ -30,10 +30,40 @@
 */
 
 #include <iostream> //iostream provides io.
-
+#include <string>   //string provides string methods.
 using namespace std;
 
+class Point {
+private:
+	int x;
+	int y; 
+public: 
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	//Print the point values of a point in x, y form.
+	friend ostream& operator<<(ostream& os, const Point& p) {
+		return os << to_string(p.x) << ", " << to_string(p.y);
+	}
+
+	//Add two Points, return a new temporary Point.
+	const Point operator+(const Point& p) const {
+		return Point(x + p.x, y + p.y);
+	}
+
+	//Subtract two Points, return a new temporary Point. 
+	const Point operator-(const Point& p) const {
+		return Point(x - p.x, y - p.y);
+	}
+};
+
+
+
 int main(void) {
-	
+	Point p1 = Point(10, 20);
+	Point p2 = Point(10, 20);
+	cout << p1 + p2 - p2 << endl;
 	return 0;
 }
