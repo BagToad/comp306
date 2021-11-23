0f47caf9f1a502aa4a4d804aa59707d90523b494
kynan <kware@protonmail.com>
2021-11-22 22:33:35 -0700

    p

--- assignment4/TMA4Question3.cpp
+++ assignment4/TMA4Question3.cpp
@@ -30,10 +30,121 @@
 */
 
 #include <iostream> //iostream provides io.
+#include <vector>   //provides vector.
+#include <set>      //provides set.
 
 using namespace std;
 
+
+/*
+	Template class to implement a generic Set class.
+	Behaves similarly to the standard C++ library set. 
+*/
+template <class T>
+class Set {
+private:	
+	vector<T> storage_vector;
+	int eos;
+public:
+	//Create an instance of Set.
+	Set() : eos(0) {}
+
+	//Declare iterator as friend class.
+	friend class iterator;
+
+	//Implement an iterator.
+	class iterator {
+	private:
+		Set& s; 
+		int index;
+	public:
+		//Create an instance of iterator pointing to first element in Set.
+		iterator(Set& s) : s(s), index(0) {}
+
+		//Create an instance of iterator pointing to last element in set. 
+		iterator(Set& s, bool b) : s(s), index(s.eos) {}
+
+		T operator*() {
+			return s.storage_vector[index];
+		}
+
+		//Prefix ++ operator for incrementing interator. 
+		T operator++() {
+			return s.storage_vector[++index];
+		}
+
+		//Postfix ++ operator for incrementing iterator.
+		T operator++(int) {
+			return s.storage_vector[index++];
+		}
+
+		//Prefix -- operator for decrementing iterator. 
+		T operator--() {
+			return s.storage_vector[--index];
+		}
+
+		//Postfix -- operator for decrementing iterator. 
+		T operator--(int) {
+			return s.storage_vector[index--];
+		}
+
+		//Comparison != operator for comparing two iterators.
+		bool operator!=(iterator& iter) {
+			return iter.index != index; 
+		}
+
+		//Comparison == operator for comparing two iterators.
+		bool operator==(iterator& iter) {
+			return iter.index == index;
+		}
+	};
+
+	iterator begin() {
+		return iterator(*this);
+	}
+
+	iterator end() {
+		return iterator(*this, true);
+	}
+
+	iterator insert(T thing) {
+		//Determine if duplicate value as required by the set interface.
+		iterator start(this->begin());
+		iterator end(this->end());
+		if (start != end) {
+			--end;
+		}
+		bool dupe = false;
+		while (start != end) {
+			if (*start == *end) {
+				dupe = true;
+				break;
+			}
+			start++;
+		}
+		// if (dupe == true) {
+		// 	return start;
+		// }
+		storage_vector.push_back(thing);
+		eos++;
+	} 
+};
+
+
 int main(void) {
-	
+	Set<string> s1;
+
+	s1.insert("Test");
+	s1.insert("test2");
+	s1.insert("test3");
+
+	Set<string>::iterator start(s1.begin());
+	Set<string>::iterator end(s1.end());
+
+	while (start != end) {
+		cout << *start << endl;
+		start++;
+	}
+
 	return 0;
 }
